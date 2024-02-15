9734732993a6d6bb8659436a8a789cfba944b1eb
Daria <daria.aleks1@yandex.ru>
2024-02-15 15:36:53 +0300

    hw3

--- app.js
+++ app.js
@@ -1,31 +1,44 @@
-// let counter = {};
-// let counter = new Counter()
-// let counter = new Object();
-// let counter = Object.assign({}, counter);
-// let counter = Object.create({});
+// Почему массив является "неправильным"?
+// Массив - это объект. Значит, что он может содержать в себе элементы любого типа, а также их размер может изменяться. Это ведет к непредсказуемому поведению. 
 
-// let counterCopy = {}
-// let counterCopy = Object.assign({}, counter)
-// for (let key in counter) {
-// 	if (counter.hasOwnProperty(key)) {
-// 		counterCopy[key] = counter[key]
-// 	}
-// }
-// let counterCopy = JSON.parse(JSON.stringify(counter))
-// let counterCopy = { ...counter }
+function logger() {
+    console.log(`I output only external context: ${this.item}`);
+  }
+const obj = { item: "some value" };
+const bindLogger = logger.bind(obj);
+bindLogger();
+logger.call(obj);
+logger.apply(obj);
 
-// function makeCounter() {
-// 	return ()
-// }
-// const makeCounter = function() {
-// 	return ()
-// }
-// const makeCounter = () => {
-// 	return ()
-// }
+const Arr = [1, 2, 3, 4, 5];
+console.log(Arr.reduce((that, next) => that + next));
 
-// structuredClone() существует для легкого создания глубоких копий значений в JavaScript.
+const stringArr = ["Hello", "world", "again", "..."];
+console.log(stringArr.join(" "));
 
-// function reverseStr(str) {
-// 	return str.split('').reverse().join('');
-// }
+const numberArr = [5, 3, 6, 8, 7, 2, 22];
+console.log(Math.max(...numberArr)); 
+console.log(Math.min(...numberArr)); 
+
+const stack = [];
+const stackIn = (i) => {
+  stack.push(i);
+  return stack;
+};
+const stackOut = () => stack.pop();
+console.log(stackIn(2));
+console.log(stackIn(4));
+console.log(stackIn(9));
+console.log(stackIn(1));
+console.log(stackOut());
+
+const queue = [];
+const queueIn = (i) => {
+  queue.unshift(i);
+  return queue;
+};
+const queueOut = () => queue.pop();
+console.log(queueIn(5));
+console.log(queueIn(9));
+console.log(queueIn(2));
+console.log(queueOut());
